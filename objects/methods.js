4d9cbedae3147bff100d478eabca035801773e9b
Julia <frosina.yu@yandex.ru>
2025-11-04 23:04:31 +0300

    Практика JS

--- objects/methods.js
+++ objects/methods.js
@@ -13,3 +13,12 @@ const person = {
 
 person.greeting();
 person.changeSurname('Smith');
+
+const correctAnswer = 'JavaScript';
+const userAnswer = ' javaScript! ';
+const isCorrect = userAnswer.toLowerCase().includes(correctAnswer.toLowerCase().trim());
+console.log(isCorrect);
+
+const salary = 15000.2085;
+const salaryFixed = salary.toFixed(2);
+console.log(salaryFixed);
